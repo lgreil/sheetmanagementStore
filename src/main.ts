8f53ea197643c7223511310cb0e13df5b9309127
Leon Greil <lgreil@uni-muenster.de>
2025-03-25 20:26:50 +0100

    Updates CORS settings to allow requests over both http and https
    Expands the origin whitelist to include both http and https URLs for the frontend and production environments, ensuring requests are allowed from different protocols.

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://bachkreis.netlify.app'], // Allow requests from frontend and production
+    origin: [
+      'http://localhost:3000', 
+      'https://localhost:3000', 
+      'http://bachkreis.netlify.app', 
+      'https://bachkreis.netlify.app'
+    ], // Allow requests from frontend and production over both http and https
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
